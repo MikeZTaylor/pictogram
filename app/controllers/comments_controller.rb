4225d233d2bd26de3e8e169833c40362ac776877
Mike Taylor <20064376@mail.wit.ie>
2016-12-28 12:27:37 +0000

    added ajax functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,31 @@
 class CommentsController < ApplicationController
 	before_action :set_post
 
-	def create  
+	def create
 		@comment = @post.comments.build(comment_params)
 		@comment.user_id = current_user.id
 
 		if @comment.save
-			flash[:success] = "You have commented on the post!"
-			redirect_to :back
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
 		else
-			flash[:alert] = "Woops, recheck the comment form, something went really wrong."
+			flash[:alert] = "Check the comment form, something went wrong."
 			render root_path
 		end
 	end
-	def destroy  
+	def destroy
 		@comment = @post.comments.find(params[:id])
 
-		@comment.destroy
-		flash[:success] = "Lets hope no one saw that"
-		redirect_to root_path
-	end  
+		if @comment.user_id == current_user.id
+			@comment.delete
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
+		end
+	end
 
 	private
 	def comment_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :user_id, presence: true
 	validates :image, presence: true
-	has_attached_file :image, styles: { :medium => "640x" }
+	validates :caption, length: { minimum: 3, maximum: 300 }
+	has_attached_file :image, styles: { :medium => "600x" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
 end
