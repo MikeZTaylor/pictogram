4b4caaafc7400155475064323c1e9d4ab3d2ccc2
Mike Taylor <20064376@mail.wit.ie>
2017-01-01 18:14:53 +0000

    added in notifications. If another user comments on your posts, a notification will be displayed in the nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy  
+
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
+
   validates :user_name, presence: true, length: { minimum: 4,
   	maximum: 20 }
-  	has_attached_file :avatar, styles: { medium: '152x152#' }  
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }  
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/  
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 	root 'posts#index' 
+
+	get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
+	get 'notifications', to: 'notifications#index' 
 	get 'profiles/show'
+	get ':user_name', to: 'profiles#show', as: :profile  
+	get ':user_name/edit', to: 'profiles#edit', as: :edit_profile  
+
 
 	devise_for :users, :controllers => { registrations: 'registrations' }
-	get ':user_name', to: 'profiles#show', as: :profile  
-	get ':user_name/edit', to: 'profiles#edit', as: :edit_profile 
+
 	patch ':user_name/edit', to: 'profiles#update', as: :update_profile 
+
 	resources :posts do  
 		resources :comments
 		member do
